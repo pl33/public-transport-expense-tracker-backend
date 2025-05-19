aded99d7f1d6b3cf12ce4c17f9763d3266e7cc94
Philipp Le <philipp@philipple.de>
2025-05-20 00:27:00 +0200

    fix(deploy): JWT Token Generation

--- make_jwt.sh
+++ make_jwt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source .env
-docker run --rm -ti -v "./data/keys/:/data/keys:ro" ghcr.io/pl33/public-transport-expense-tracker-backend:latest token --key-dir /data/keys create-token --issuer $JWT_ISSUER -e $(date --utc --date="+364 days" +%FT%XZ) --claims-json '{"ptet:write":true}' $1
+docker run --rm -ti -v "./data/keys/:/data/keys:ro" ghcr.io/pl33/public-transport-expense-tracker-backend:latest token --key-dir /data/keys create-token --issuer $JWT_ISSUER --audience $BASE_URI -e $(date --utc --date="+364 days" +%FT%XZ) --claims-json '{"ptet:write":true}' $1
